f7e58cca3966d79b2f6a42f8dfb8ab733e6fe508
Awad Osman <awadosman997@gmail.com>
2024-11-11 13:59:13 +0100

    completed LSMS/Tanzania dataset

--- utils/constants.py
+++ utils/constants.py
@@ -10,6 +10,22 @@ SOLAR_HOME_SYSTEM = 'Solar Home System'
 SOLAR_HOME_SYSTEM_SOLAR_PV = 'Solar home system (Solar PV system)'
 ALWAYS_HAD_GRID = 'HH has always had grid connection'
 
-
 Village_variables = ['Climate1', 'City_dist', 'Grid_dist', 'Latitude', 'Longitude', 'masl', 'Population']
 GADM_variables = ['Climate_majority', 'Population_mean', 'Relative_wealth_index', 'masl_mean']
+
+DRIVERS_LIST = ['Years_of_HHH_in_community',
+                'Dwelling_quality_index', 'Hours_available_electricity',
+                'Measurement_age', 'Monthly_expenditure', 'Number_of_rooms',
+                'Climate_zone_lev_1', 'Climate_zone_lev_2',
+                'Age_HHH', 'HH_with_home_business',
+                'Education_level_HHH', 'Socio_status_HHH', 'Number_adults',
+                'Ownership_motorized_vehicle', 'Ownership_small_livestock',
+                'Ownership_large_livestock', 'Clean_fuel', 'Tariff_payment_frequency'
+                ]
+
+PRESENCE_LIST = [
+    'Presence_refrigerator/freezer', 'Presence_iron', 'Presence_fan',
+    'Presence_DVD_player', 'Presence_radio/stereo',
+    'Presence_phone_charger',
+    'Presence_TV'
+]
